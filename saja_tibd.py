b0571f6d9de85bb25faa32af9e6b110bd9516af2
Salshafeay <78335103+Salshafeay@users.noreply.github.com>
2021-10-27 12:34:41 -0500

    Fixed model and added 2 rules.

--- saja_tibd.py
+++ saja_tibd.py
@@ -1,7 +1,7 @@
 from pysb import *
 from pysb.simulator import ScipyOdeSimulator
 import numpy as np
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 Model()
 
@@ -13,25 +13,50 @@ Model()
 # simulation commands
 
 
-# (T) tumor cell monomer
-# (B) osteoblast monomer
-# (C) osteoclast monomer
-# (P) PTHrP monomer
-# (Beta) TGF-Beta monomer
 
 
-Monomer('T')
-Monomer('P')
-Monomer('B')
-Monomer('C')
-Monomer('Beta')
+
+Monomer('T') # Tumor cell
+Monomer('P') # (P) PTHrP monomer
+Monomer('B', ['state'], {'state': ['x','L']}) # (B) osteoblast monomer
+Monomer('C') # (C) osteoclast monomer
+Monomer('Beta') # (Beta) TGF-Beta monomer
 
 Parameter('T_init',100)
-initial(T(T >> T + P) , T_init)
+Initial(T(), T_init)
+Parameter('B_init', 100)
+Initial(B(state='x'), B_init)
+
+Parameter('ktp_tumor', 0.05)
+Rule('PTHrP_production' , T() >> T() + P() , ktp_tumor )
+
+Parameter('k_BX_BL', 0.05)
+Rule('Ob_express_RANKL' , P() + B(state='x') >> P() + B(state='L') , k_BX_BL)
 
 
-parameter('ktp_tumor', 1)
-Rule('tumor' , T >> T + P , ktp_tumor )
+Observable('Tumor_cells',T())
+Observable('PTHrP', P())
+Observable('BL' , B(state='L'))
+Observable('BX', B(state='x'))
+
+
+tspan = np.linspace(0, 10, 1001)
+sim = ScipyOdeSimulator(model, tspan, verbose=True)
+result = sim.run()
+
+obs = result.observables
+print(model.observables)
+for o in model.observables:
+    plt.plot(tspan, obs[o.name] , lw=2, label=o.name)
+
+plt.xlabel('time')
+plt.ylabel('concentration')
+plt.legend(loc=0)
+
+plt.show()
+
+
+
 
 
 
