e7955a65837bd439a3c0d8eb07f1761ae933b277
Salshafeay <78335103+Salshafeay@users.noreply.github.com>
2021-11-01 16:51:20 -0500

    Adding 3rd rule.

--- saja_tibd.py
+++ saja_tibd.py
@@ -19,13 +19,15 @@ Model()
 Monomer('T') # Tumor cell
 Monomer('P') # (P) PTHrP monomer
 Monomer('B', ['state'], {'state': ['x','L']}) # (B) osteoblast monomer
-Monomer('C') # (C) osteoclast monomer
+Monomer('C', ['state'] , {'state': ['c','cc']}) # (C) osteoclast monomer
 Monomer('Beta') # (Beta) TGF-Beta monomer
 
 Parameter('T_init',100)
 Initial(T(), T_init)
 Parameter('B_init', 100)
 Initial(B(state='x'), B_init)
+Parameter('C_init', 100)
+Initial(C(state='cc'), C_init)
 
 Parameter('ktp_tumor', 0.05)
 Rule('PTHrP_production' , T() >> T() + P() , ktp_tumor )
@@ -33,11 +35,16 @@ Rule('PTHrP_production' , T() >> T() + P() , ktp_tumor )
 Parameter('k_BX_BL', 0.05)
 Rule('Ob_express_RANKL' , P() + B(state='x') >> P() + B(state='L') , k_BX_BL)
 
+Parameter('k_C_cc', 1)
+Rule('Oc_doupled' , B(state='L') + C(state='c') >> B(state='L') + C(state='cc') , k_C_cc)
+
 
 Observable('Tumor_cells',T())
 Observable('PTHrP', P())
 Observable('BL' , B(state='L'))
 Observable('BX', B(state='x'))
+Observable('Cc', C(state='c'))
+Observable('C2c', C(state='cc'))
 
 
 tspan = np.linspace(0, 10, 1001)
