6c4eb495a5e972f58e1595cf34b21f87f0b0f202
Salshafeay <78335103+Salshafeay@users.noreply.github.com>
2021-11-04 18:05:41 -0500

    Adding 6 rules.

--- saja_tibd.py
+++ saja_tibd.py
@@ -20,14 +20,21 @@ Monomer('T') # Tumor cell
 Monomer('P') # (P) PTHrP monomer
 Monomer('B', ['state'], {'state': ['x','L']}) # (B) osteoblast monomer
 Monomer('C') # (C) osteoclast monomer
-Monomer('Beta') # (Beta) TGF-Beta monomer
+Monomer('Beta', ['state'], {'state': ['a','i']}) # (Beta) TGF-Beta monomer
 
 Parameter('T_init',100)
 Initial(T(), T_init)
 Parameter('B_init', 100)
 Initial(B(state='x'), B_init)
 Parameter('C_init', 100)
-Initial(C(state='cc'), C_init)
+Initial(C(), C_init)
+Parameter('betaa_init', 100)
+Initial(Beta(state='a') , betaa_init)
+Parameter('Beta_init', 100)
+Initial(Beta(state='i'), Betai_init)
+Parameter('B_init', 100)
+Initial(B(), B_init)
+
 
 Parameter('ktp_tumor', 0.05)
 Rule('PTHrP_production' , T() >> T() + P() , ktp_tumor )
@@ -36,20 +43,45 @@ Parameter('k_BX_BL', 0.05)
 Rule('Ob_express_RANKL' , P() + B(state='x') >> P() + B(state='L') , k_BX_BL)
 
 Parameter('k_C_cc', 1)
-Rule('Oc_doupled' , B(state='L') + C() >> B(state='L') + C() + C(), k_C_cc)
+Rule('Oc_interact_Ob' , B(state='L') + C() >> B(state='L') + C() + C(), k_C_cc)
+
+Parameter('k_Oc_Beta', 1)
+Rule('Oc_express_TGFB', C() >> C() + Beta(state='a') , k_Oc_Beta)
+
+
+Parameter('K_Beta_tumor' , 1)
+Rule('Beta_binds_T', Beta(state='a') + T() >> Beta(state='a') + T() + T() , K_Beta_tumor)
 
 Parameter('k_T_div', 1)
 Rule('T_divides', T() >> T() + T(), k_T_div)
 
+Parameter('k_OC_div', 1)
+Rule('T_divides', C() >> C() + C(), k_OC_div)
+
+Parameter('k_OB_div', 1)
+Rule('T_divides', B() >> B() + B(), k_OB_div)
+
 Parameter('k_T_death', 1)
 Rule('T_dies', T() >> None, k_T_death)
+
+Parameter('k_OC_death', 1)
+Rule('OC_dies', C() >> None, K_OC_death)
+
+Parameter('k_OB_death', 1)
+Rule('OB_dies', B() >> None, k_OB_death)
+
+
 
 Observable('Tumor_cells',T())
 Observable('PTHrP', P())
 Observable('BL' , B(state='L'))
 Observable('BX', B(state='x'))
-Observable('Cc', C(state='c'))
-Observable('C2c', C(state='cc'))
+Observable('OC', C())
+Observable('OB', B())
+Observable('TGF_Beta_A',Beta(state='a'))
+Observable('TGF_Beta_I', Beta(state='i'))
+
+
 
 
 tspan = np.linspace(0, 10, 1001)
@@ -72,4 +104,3 @@ plt.show()
 
 
 
-#Rule('P_binds_B',
