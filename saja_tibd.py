62f2f84cc94da749de868b180f2f3bec98948cff
lh64 <lh64@cornell.edu>
2021-11-05 11:15:41 -0500

    Made two rules for Osteoblast division to represent division in the unligated and ligated states. Also played around with some parameters to get something that will run without causing an integration error

--- saja_tibd.py
+++ saja_tibd.py
@@ -12,43 +12,37 @@ Model()
 # Observables
 # simulation commands
 
+Monomer('T')  # Tumor cell
+Monomer('P')  # (P) PTHrP monomer
+Monomer('B', ['state'], {'state': ['x', 'L']})  # (B) osteoblast monomer
+Monomer('C')  # (C) osteoclast monomer
+Monomer('Beta', ['state'], {'state': ['a', 'i']})  # (Beta) TGF-Beta monomer
 
-
-
-
-Monomer('T') # Tumor cell
-Monomer('P') # (P) PTHrP monomer
-Monomer('B', ['state'], {'state': ['x','L']}) # (B) osteoblast monomer
-Monomer('C') # (C) osteoclast monomer
-Monomer('Beta', ['state'], {'state': ['a','i']}) # (Beta) TGF-Beta monomer
-
-Parameter('T_init',100)
+Parameter('T_init', 100)
 Initial(T(), T_init)
 Parameter('B_init', 100)
 Initial(B(state='x'), B_init)
 Parameter('C_init', 100)
 Initial(C(), C_init)
 Parameter('betaa_init', 100)
-Initial(Beta(state='a') , betaa_init)
+Initial(Beta(state='a'), betaa_init)
 Parameter('betai_init', 100)
 Initial(Beta(state='i'), betai_init)
 
-
 Parameter('ktp_tumor', 1)
-Rule('PTHrP_production' , T() >> T() + P() , ktp_tumor )
+Rule('PTHrP_production', T() >> T() + P(), ktp_tumor)
 
 Parameter('k_BX_BL', 1)
-Rule('Ob_express_RANKL' , P() + B(state='x') >> P() + B(state='L') , k_BX_BL)
+Rule('Ob_express_RANKL', P() + B(state='x') >> P() + B(state='L'), k_BX_BL)
 
-Parameter('k_C_cc', 1)
+Parameter('k_C_cc', 0.1)
 Rule('Oc_interact_Ob' , B(state='L') + C() >> B(state='L') + C() + C(), k_C_cc)
 
 Parameter('k_Oc_Beta', 1)
-Rule('Oc_express_TGFB', C() >> C() + Beta(state='a') , k_Oc_Beta)
+Rule('Oc_express_TGFB', C() >> C() + Beta(state='a'), k_Oc_Beta)
 
-
-Parameter('K_Beta_tumor' , 1)
-Rule('Beta_binds_T', Beta(state='a') + T() >> Beta(state='a') + T() + T() , K_Beta_tumor)
+Parameter('K_Beta_tumor', 0.1)
+Rule('T_divides_beta', Beta(state='a') + T() >> Beta(state='a') + T() + T(), K_Beta_tumor)
 
 Parameter('k_T_div', 1)
 Rule('T_divides', T() >> T() + T(), k_T_div)
@@ -57,41 +51,41 @@ Parameter('k_OC_div', 1)
 Rule('C_divides', C() >> C() + C(), k_OC_div)
 
 Parameter('k_OB_div', 1)
-Rule('B_divides', B() >> B() + B(), k_OB_div)
+Rule('Bx_divides', B(state='x') >> B(state='x') + B(state='x'), k_OB_div)
+Rule('BL_divides', B(state='L') >> B(state='L') + B(state='L'), k_OB_div)
 
-Parameter('k_T_death',1)
+Parameter('k_T_death', 10)
 Rule('T_dies', T() >> None, k_T_death)
 
-Parameter('k_OC_death', 1)
+Parameter('k_OC_death', 10)
 Rule('OC_dies', C() >> None, k_OC_death)
 
 Parameter('k_OB_death', 1)
 Rule('OB_dies', B() >> None, k_OB_death)
 
-Parameter('k_PTHrP_death', 1)
-Rule('PTHrP_dies', P() >> None, k_PTHrP_death)
+Parameter('k_PTHrP_deg', 1)
+Rule('PTHrP_degrades', P() >> None, k_PTHrP_deg)
 
-Parameter('k_TGF_death', 1)
-Rule('TGF_dies', Beta() >> None, k_TGF_death)
+Parameter('k_TGF_deg', 1)
+Rule('TGF_degrades', Beta() >> None, k_TGF_deg)
 
-
-Observable('Tumor_cells',T())
+Observable('Tumor_cells', T())
 Observable('PTHrP', P())
-Observable('BL' , B(state='L'))
+Observable('BL', B(state='L'))
 Observable('BX', B(state='x'))
 Observable('OC', C())
 Observable('OB', B())
-Observable('TGF_Beta_A',Beta(state='a'))
+Observable('TGF_Beta_A', Beta(state='a'))
 Observable('TGF_Beta_I', Beta(state='i'))
 
-tspan = np.linspace(0, 10, 1001)
+tspan = np.linspace(0, 1, 1001)
 sim = ScipyOdeSimulator(model, tspan, verbose=True)
 result = sim.run()
 
 obs = result.observables
 #print(model.observables)
 for o in model.observables:
-    plt.plot(tspan, obs[o.name] , lw=2, label=o.name)
+    plt.plot(tspan, obs[o.name], lw=2, label=o.name)
 
 plt.xlabel('time')
 plt.ylabel('concentration')
