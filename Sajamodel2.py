1f8d006dc28e8810b67ea62d398944dabf93ac9b
Salshafeay <78335103+Salshafeay@users.noreply.github.com>
2022-07-28 12:25:13 -0500

    Fixed ROB and AOB parameters

--- Sajamodel2.py
+++ Sajamodel2.py
@@ -14,10 +14,10 @@ Model()
 
 Monomer('UP')  # uncommitted Progenitors
 Monomer('OPG') # Osteoprotegerin
-Monomer('PTH', ['rob'])  # (PTH) PTH monomer
-Monomer('ROB', ['pth'])  # (ROB) Responding osteoblast monomer
-Monomer('AOB', ['state'], {'state': ['X','L']})  # (AOB) Active osteoblast monomer
-Monomer('TGFB')  # (TGF-B) TGF-Beta monomer
+Monomer('PTH', ['ob'])  # (PTH) PTH monomer
+Monomer('ROB', ['pth','tgfb'])  # (ROB) Responding osteoblast monomer
+Monomer('AOB', ['pth','state'], {'state': ['X','L']})  # (AOB) Active osteoblast monomer
+Monomer('TGFB',['ob'])  # (TGF-B) TGF-Beta monomer
 Monomer('AOC') # Active Osteoclast monomer
 Monomer('OCP') # Osteoclast Precursors
 
@@ -30,16 +30,16 @@ Parameter('OPG_init', 0)
 Initial(OPG(), OPG_init)
 
 Parameter('PTH_init', 0)
-Initial(PTH(rob=None), PTH_init)
+Initial(PTH(ob=None), PTH_init)
 
 Parameter('ROB_init', 0)
-Initial(ROB(pth=None), ROB_init)
+Initial(ROB(pth=None,tgfb=None), ROB_init)
 
 Parameter('AOB_init', 0)
-Initial(AOB(state='X'), AOB_init)
+Initial(AOB(pth=None,state='X'), AOB_init)
 
 Parameter('TGFB_init', 0)
-Initial(TGFB(), TGFB_init)
+Initial(TGFB(ob=None), TGFB_init)
 
 Parameter('AOC_init', 0)
 Initial(AOC(), AOC_init)
@@ -50,17 +50,27 @@ Rule('ROB_production', UP() + TGFB() >> ROB() + TGFB(), kf_UP_ROB)
 
 Parameter('kf_ROB_PTH',1)
 Parameter('kr_ROB_PTH',1)
-Rule('ROB_BIND_PTH', ROB(pth=None) + PTH(rob=None) | ROB(pth=1) % PTH(rob=1) ,kf_ROB_PTH, kr_ROB_PTH )
-
+Rule('ROB_BIND_PTH', ROB(pth=None) + PTH(ob=None) | ROB(pth=1) % PTH(ob=1) ,kf_ROB_PTH, kr_ROB_PTH )
 
 Parameter('kf_AOB_PTH', 1)
-Rule('AOB_Bind_PTH', AOB() + PTH() | AOB() + PTH(), kf_AOB_PTH)
+Parameter('kr_AOB_PTH', 1)
+Rule('AOB_Bind_PTH', AOB(pth=None) + PTH(ob=None) | AOB(pth=1) % PTH(ob=1), kf_AOB_PTH, kr_AOB_PTH)
 
-Parameter('kf_ROB_AOB',1)
-Rule('AOB_Production', ROB() + TGFB() >> AOB() + TGFB(), kf_ROB_AOB)
+Parameter('k_AOB_L',1)
+Rule('AOB_EXPRESS_L', AOB(pth=1,state='X') % PTH(ob=1) >> AOB(pth=1, state='L') % PTH(ob=1), k_AOB_L)
 
-Parameter('kf_AOB_L',1)
-Rule('AOB_EXPRESS_L', AOB(state='X') + PTH() | AOB(state='L') + PTH(), kf_AOB_L)
+Parameter('k_AOB_L_X',1)
+Rule('AOB_L_TO_X', AOB(pth=None,state='L') >> AOB(pth=None,state='X'), k_AOB_L_X)
+
+Parameter('k_ROB_AOB',1)
+Rule('AOB_Production', ROB(pth=None,tgfb=None) >> AOB(pth=None,state='X'), k_ROB_AOB)
+
+Parameter('k_ROB_AOB_pth',1)
+Rule('AOB_Production_pth', ROB(pth=ANY,tgfb=None) >> AOB(pth=1,state='X') % PTH(ob=1), k_ROB_AOB_pth)
+
+Parameter('kf_ROB_TGFB',1)
+Parameter('kr_ROB_TGFB',1)
+Rule('ROB_BIND_TGFB', ROB(tgfb=None) + TGFB(ob=None) | ROB(tgfb=1) % TGFB(ob=1) ,kf_ROB_TGFB, kr_ROB_TGFB )
 
 Parameter('k_AOB_death',1)
 Rule('AOB_death', AOB() >> None, k_AOB_death)
@@ -72,9 +82,12 @@ Parameter('k_OPG_deg',1)
 Rule('OPG_deg', OPG() >> None, k_OPG_deg)
 
 Parameter('kf_AOB_L_OPG',1)
-Rule('AOB_EXPRESS_L', AOB(state='X') + OPG() | AOB(state='L') + OPG(), kf_AOB_L_OPG)
+Parameter('kr_AOB_L_OPG',1)
+Rule('AOB_EXPRESS_L_OPG', AOB(state='X') + OPG() | AOB(state='L') + OPG(), kf_AOB_L_OPG, kr_AOB_L_OPG)
 
 Parameter('kf_AOB_L_PTH_OPG',1)
-Rule('AOB_EXPRESS_L', AOB(state='X') + OPG()+ PTH() | AOB(state='L') + OPG() +PTH(), kf_AOB_L_PTH_OPG)
+Parameter('kr_AOB_L_PTH_OPG',1)
+Rule('AOB_EXPRESS_L_OPG_PTH', AOB(state='X') + OPG()+ PTH() | AOB(state='L') + OPG() +PTH(), kf_AOB_L_PTH_OPG, kr_AOB_L_PTH_OPG)
 
-
+#Parameter('',1)
+#Rule(''
