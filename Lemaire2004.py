feb5143ab8f6a9f9b24a06ec941d7d8c7cb48285
lh64 <lh64@cornell.edu>
2023-02-07 22:28:04 -0600

    Made some small changes to Lemaire2004.py: 1) cleaned up the code a bit to eliminate warnings from PyCharm, 2) changed the names of some variables to match those in the paper, and 3) added a few observables

--- Lemaire2004.py
+++ Lemaire2004.py
@@ -13,25 +13,24 @@ Model()
 # simulation commands
 
 # Monomers
-Monomer('P',['pr'])
-Monomer('Pr',['p'])
-Monomer('O',['l'])
-Monomer('L',['k'])
-Monomer('K',['l'])
+Monomer('P', ['pr'])
+Monomer('Pr', ['p'])
+Monomer('O', ['l'])
+Monomer('L', ['k'])
+Monomer('K', ['l'])
 
-Parameter('P_init',100)
-Parameter('Pr_init',100)
-Parameter('O_init',100)
-Parameter('L_init',100)
-Parameter('K_init',100)
+Parameter('P_init', 100)
+Parameter('Pr_init', 100)
+Parameter('O_init', 100)
+Parameter('L_init', 100)
+Parameter('K_init', 100)
 
-#Initials
+# Initials
 Initial(P(pr=None), P_init)
 Initial(Pr(p=None), Pr_init)
 Initial(O(l=None), O_init)
 Initial(L(k=None), L_init)
 Initial(K(l=None),K_init)
-
 
 '''
 In the the paper there is number of receptors per cell
@@ -41,68 +40,62 @@ R(p,t), stands for number of receptor per cell, it a constant
 
 '''
 
-#Parameters
+# Parameters for P + Pr
 Parameter('Sp', 1)
 Parameter('Ip', 1)
 Parameter('kp', 1)
 Parameter('k5', 1)
 Parameter('k6', 1)
-# Reaction schemes of binding of OPG-RANKL and RANK-RANKL
+
+# Parameters for O + L
 Parameter('po', 1)
-Parameter('do', 1)
-Parameter('k1', 1)
-Parameter('k2', 1)
+Parameter('ko', 1)
 Parameter('pl', 1)
 Parameter('dl', 1)
+Parameter('k1', 1)
+Parameter('k2', 1)
 
-#Rules
-Rule('P_creation_basal',None >>P(pr=None), Sp)
-Rule('P_creation_external',None>>P(pr=None), Ip)
-
-Rule('P_destruction',P(pr=None) >> None, kp)
-Rule('P_binds_PR',P(pr=None) + Pr(p=None) | P(pr=1) % Pr(p=1),k5,k6)
-# Rules for O + L
-Rule('O_creation', None >> O(l=None),po)
-Rule('O_destruction', O(l=None) >> None,do)
-Rule('O_bind_L',O(l=None) + L(k=None) | O(l=1) % L(k=1),k1,k2)
-Rule('L_creation',None >> L(k=None),pl)
-Rule('L_destruction',L(k=None) >>None,dl)
-#Rules for L + K
-# where does K comes from?
-'''
-k is not considered a model variable
-
-'''
+# Parameters for K + L
 Parameter('k3', 1)
-Parameter('k4',1)
-Rule('K_binds_to_L',K(l=None) +L(k=None) | K(l=1) % L(k=1),k3,k4)
+Parameter('k4', 1)
 
+# Rules for P + Pr
+Rule('P_creation_basal', None >> P(pr=None), Sp)
+Rule('P_creation_external', None >> P(pr=None), Ip)
+Rule('P_destruction', P(pr=None) >> None, kp)
+Rule('P_binds_Pr', P(pr=None) + Pr(p=None) | P(pr=1) % Pr(p=1), k5, k6)
 
-#Observables
+# Rules for O + L
+Rule('O_creation', None >> O(l=None), po)
+Rule('O_destruction', O(l=None) >> None, ko)
+Rule('O_binds_L', O(l=None) + L(k=None) | O(l=1) % L(k=1), k1, k2)
 
-# Do we want to see Overall PTH production(ONLY?)
-# Do we want to see the
+# Rules for L + K
+Rule('L_creation', None >> L(k=None), pl)
+Rule('L_destruction', L(k=None) >>None, dl)
+Rule('K_binds_to_L', K(l=None) + L(k=None) | K(l=1) % L(k=1), k3, k4)
 
+# Observables
 Observable('P_free', P(pr=None))
 Observable('Pr_free', Pr(p=None))
 Observable('O_free', O(l=None))
 Observable('L_free', L(k=None))
 Observable('K_free', K(l=None))
+Observable('P_bound_Pr', P(pr=1) % Pr(p=1))
+Observable('O_bound_L', O(l=1) % L(k=1))
+Observable('K_bound_L', K(l=1) % L(k=1))
 
+# Run simulation
 
-tspan=np.linspace(0,100,101)
-sim=ScipyOdeSimulator(model,tspan,verbose=True)
-result=sim.run()
+tspan = np.linspace(0, 100, 101)
+sim = ScipyOdeSimulator(model, tspan, verbose=True)
+result = sim.run()
 
 for obs in model.observables:
     plt.plot(tspan, result.observables[obs.name], lw=2, label=obs.name)
-
 
 plt.xlabel('time')
 plt.ylabel('concentration')
 plt.legend(loc='best')
 
 plt.show()
-
-
-
