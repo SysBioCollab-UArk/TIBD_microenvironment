17e04385f5faa19ba808e6fce60fbb587cdfaf8d
atgvega28 <alextvg28@gmail.com>
2023-02-17 18:46:19 -0600

    Added rate constants from the Lemaire 2004 paper and compare simulation results to an equilibrium equation from the  paper.

--- Lemaire2004.py
+++ Lemaire2004.py
@@ -19,14 +19,31 @@ Monomer('O',['l'])
 Monomer('L',['k'])
 Monomer('K',['l'])
 
-Parameter('P_init',100)
-Parameter('Pr_init',100)
-Parameter('O_init',100)
-Parameter('L_init',100)
-Parameter('K_init',100)
+
+R_0 = 0.0007734 #pM Initial Number of ROB's
+B_0 = 0.0007282 #pM Initial Number of AOB's
+
+Parameter('Sp', 250) #pM/day
+Parameter('Ip', 0)# pM/day range from 0 to 10^6
+Parameter('kp', 86) # 1/day
+Parameter('k5', 0.02)# 1/pM-day
+Parameter('k6', 3) #1/day
+
+Pbar = (Sp.value + Ip.value) / kp.value
+pi_P = Pbar / (Pbar + k6.value/k5.value)
+RTP = 100 # Total Number of PTH receptors
+
+
+Parameter('P_init', 100)
+Parameter('PrP_init', 0) #pi_P * RTP)
+Parameter('Pr_init', RTP) # RTP- PrP_init.value)
+Parameter('O_init', 100)
+Parameter('L_init', 100)
+Parameter('K_init', 100)
 
 #Initials
 Initial(P(pr=None), P_init)
+Initial(Pr(p=1) % P(pr=1), PrP_init)
 Initial(Pr(p=None), Pr_init)
 Initial(O(l=None), O_init)
 Initial(L(k=None), L_init)
@@ -42,11 +59,7 @@ R(p,t), stands for number of receptor per cell, it a constant
 '''
 
 #Parameters
-Parameter('Sp', 1)
-Parameter('Ip', 1)
-Parameter('kp', 1)
-Parameter('k5', 1)
-Parameter('k6', 1)
+
 # Reaction schemes of binding of OPG-RANKL and RANK-RANKL
 Parameter('po', 1)
 Parameter('do', 1)
@@ -55,6 +68,8 @@ Parameter('k2', 1)
 Parameter('pl', 1)
 Parameter('dl', 1)
 
+
+
 #Rules
 Rule('P_creation_basal',None >>P(pr=None), Sp)
 Rule('P_creation_external',None>>P(pr=None), Ip)
@@ -62,11 +77,11 @@ Rule('P_creation_external',None>>P(pr=None), Ip)
 Rule('P_destruction',P(pr=None) >> None, kp)
 Rule('P_binds_PR',P(pr=None) + Pr(p=None) | P(pr=1) % Pr(p=1),k5,k6)
 # Rules for O + L
-Rule('O_creation', None >> O(l=None),po)
-Rule('O_destruction', O(l=None) >> None,do)
-Rule('O_bind_L',O(l=None) + L(k=None) | O(l=1) % L(k=1),k1,k2)
-Rule('L_creation',None >> L(k=None),pl)
-Rule('L_destruction',L(k=None) >>None,dl)
+# Rule('O_creation', None >> O(l=None),po)
+# Rule('O_destruction', O(l=None) >> None,do)
+# Rule('O_bind_L',O(l=None) + L(k=None) | O(l=1) % L(k=1),k1,k2)
+# Rule('L_creation',None >> L(k=None),pl)
+# Rule('L_destruction',L(k=None) >>None,dl)
 #Rules for L + K
 # where does K comes from?
 '''
@@ -74,8 +89,8 @@ k is not considered a model variable
 
 '''
 Parameter('k3', 1)
-Parameter('k4',1)
-Rule('K_binds_to_L',K(l=None) +L(k=None) | K(l=1) % L(k=1),k3,k4)
+Parameter('k4', 1)
+# Rule('K_binds_to_L',K(l=None) +L(k=None) | K(l=1) % L(k=1),k3,k4)
 
 
 #Observables
@@ -85,12 +100,17 @@ Rule('K_binds_to_L',K(l=None) +L(k=None) | K(l=1) % L(k=1),k3,k4)
 
 Observable('P_free', P(pr=None))
 Observable('Pr_free', Pr(p=None))
-Observable('O_free', O(l=None))
-Observable('L_free', L(k=None))
-Observable('K_free', K(l=None))
+Observable('Pr_P_bound', Pr(p=1) % P(pr=1))
+# Observable('O_free', O(l=None))
+# Observable('L_free', L(k=None))
+# Observable('K_free', K(l=None))
 
 
-tspan=np.linspace(0,100,101)
+Expression('piP_actual', Pr_P_bound / (Pr_free + Pr_P_bound))
+Expression('piP_theory', P_free / (P_free + k6/k5))
+
+
+tspan=np.linspace(0,.2, 101)
 sim=ScipyOdeSimulator(model,tspan,verbose=True)
 result=sim.run()
 
@@ -100,6 +120,14 @@ for obs in model.observables:
 
 plt.xlabel('time')
 plt.ylabel('concentration')
+plt.legend(loc='best')
+
+plt.figure()
+for exp in model.expressions:
+    plt.plot(tspan, result.expressions[exp.name], lw=2, label=exp.name)
+    # print(exp)
+plt.xlabel('time')
+plt.ylabel('value')
 plt.legend(loc='best')
 
 plt.show()
