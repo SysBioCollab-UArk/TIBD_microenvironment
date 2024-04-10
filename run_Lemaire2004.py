1f21ad96c05736b840c9416d5c4660cf31874301
lh64 <lh64@cornell.edu>
2024-04-10 01:30:38 -0500

    Modified the equations for OB bone production and OC bone consumption. I've actually implemented two different models that we should analyze. I think the first model is more robust but we'll need to do a thorough investigation to know for sure. Also adding a spreadsheet with some rate calculations I made to get preliminary parameter values for the 2nd model.

--- run_Lemaire2004.py
+++ run_Lemaire2004.py
@@ -15,27 +15,66 @@ exp_data = get_exp_data(os.path.join('DATA', 'Mouse_Data_March2024.csv'))
 
 Monomer('Bone')
 Parameter('Bone_0', 100)  # percentage
-# TODO: make OB production rate increase with decreasing OB concentration
-# Parameter('k_B_builds_bone', 1.25e8)  # /day
-Parameter('k1_B_builds_bone', 1.25e8)  # /day
+
+# Make OB bone production (OC bone consumption) rate increase (decrease) with decreasing (increasing) concentration
+# === MODEL #1 ===
+# '''
+Parameter('k1_B_builds_bone', 0)  # /day 1.25e8
 Parameter('k2_B_builds_bone', 100)  # fM
-Parameter('nB', 1.1)
+Parameter('nB', 1)  # 1.1
+Parameter('k1_C_consumes_bone', 1e6)  # /day
+Parameter('k2_C_consumes_bone', 1)  # fM
+Parameter('nC', 1)  # 0.8
 alias_model_components()
 Expression('k_B_builds_bone',
            Piecewise((0, B_obs <= 0),
-                     (k1_B_builds_bone * (k2_B_builds_bone + B_obs ** nB) / B_obs ** nB, True)))
-# TODO: make OC consumption rate decrease with increasing OC concentration
-# Parameter('k_C_consumes_bone', 1e6)  # /day
-Parameter('k1_C_consumes_bone', 1e6)  # /day
-Parameter('k2_C_consumes_bone', 100)  # fM
-Parameter('nC', 0.8)
-alias_model_components()
+                     (k1_B_builds_bone * (k2_B_builds_bone ** nB + B_obs ** nB) / B_obs ** nB, True)))
 Expression('k_C_consumes_bone',
            Piecewise((0, C_obs <= 0),
-                     (k1_C_consumes_bone * C_obs ** nC / (k2_C_consumes_bone + C_obs ** nC), True)))
+                     (k1_C_consumes_bone * (k2_C_consumes_bone ** nC + C_obs ** nC) / C_obs ** nC, True)))
+
+# estimate rate constant for OB -> OB + Bone to get bone density ~ 100
+mean_OB = 1.06E-02 * 1000  # fM, from data
+mean_OC = 9.16E-04 * 1000  # fM, from data
+k1C = k1_C_consumes_bone.value
+k2C = k2_C_consumes_bone.value
+k2B = k2_B_builds_bone.value
+k1_B_builds_bone.value = k1C * (k2C ** nC.value + mean_OC ** nC.value) / mean_OC ** (nC.value-1) * Bone_0.value * \
+                         mean_OB ** (nB.value - 1) / (k2B ** nB.value + mean_OB ** nB.value) * \
+                         1.33  # 1.8
+kB_label = r'$k_{1B} \cdot \frac{k_{2B}^{n_B} + [B]^{n_B}}{[B]^{n_B}}$'
+kC_label = r'$k_{1C} \cdot \frac{k_{2C}^{n_C} + [C]^{n_C}}{[C]^{n_C}}$'
+# '''
+# === MODEL #2 ===
+'''
+Parameter('k1_B_builds_bone', 2.3e6)  # /day
+Parameter('k2_B_builds_bone', 0.4)  # fM 0.8
+Parameter('nB', 1.7)  # 2
+Parameter('kB_star', 1.4e9)  # /day
+Parameter('k1_C_consumes_bone', 1e6)  # /day
+Parameter('k2_C_consumes_bone', 1)  # fM
+Parameter('nC', 1)  # 0.8
+Parameter('kC_star', 2e6)  # /day
+alias_model_components()
+Expression('k_B_builds_bone',
+           k1_B_builds_bone + kB_star * k2_B_builds_bone ** nB / (k2_B_builds_bone ** nB + B_obs ** nB))
+Expression('k_C_consumes_bone',
+           k1_C_consumes_bone + kC_star * k2_C_consumes_bone ** nC / (k2_C_consumes_bone ** nC + C_obs ** nC))
+
+# estimate rate constant for OB -> OB + Bone to get bone density ~ 100
+mean_OB = 1.06E-02 * 1000  # fM, from data
+mean_OC = 9.16E-04 * 1000  # fM, from data
+k1C = k1_C_consumes_bone.value
+k2C = k2_C_consumes_bone.value
+k2B = k2_B_builds_bone.value
+k1_B_builds_bone.value = ((k1C + kC_star.value * k2C ** nC.value / (k2C ** nC.value + mean_OC ** nC.value)) * mean_OC * \
+                         Bone_0.value / mean_OB - kB_star.value * k2B ** nB.value / (k2B ** nB.value + mean_OB ** nB.value)) * \
+                         1.93
+kB_label = r'$k_{1B} + k_B^\ast \cdot \frac{k_{2B}^{n_B}}{k_{2B}^{n_B} + [B]^{n_B}}$'
+kC_label = r'$k_{1C} + k_C^\ast \cdot \frac{k_{2C}^{n_C}}{k_{2C}^{n_C} + [C]^{n_C}}$'
+'''
 
 alias_model_components()
-
 Initial(Bone(), Bone_0)
 Observable('Bone_tot', Bone())
 Rule('B_builds_bone', B() >> B() + Bone(), k_B_builds_bone)
@@ -54,18 +93,15 @@ Parameter('N', 600)  # fM
 # allee effect
 Parameter('ALLEE_ON', 0)
 Parameter('A', 1.5)  # 1/fM
-
 alias_model_components()
 
 Observable('Tumor_tot', Tumor())
-
 alias_model_components()
 
 Expression('k_tumor_cc', CC_ON * (k_tumor_div - k_tumor_dth) / N)
 Expression('k_tumor_allee',
            Piecewise((0, Tumor_tot <= 0),
                      (ALLEE_ON * (k_tumor_div - k_tumor_dth) / (A * Tumor_tot), True)))
-
 alias_model_components()
 
 Initial(Tumor(), Tumor_0)
@@ -74,8 +110,7 @@ Rule('tumor_death', Tumor() >> None, k_tumor_dth)
 Rule('tumor_cc', Tumor() + Tumor() >> Tumor(), k_tumor_cc)
 Rule('tumor_allee', Tumor() >> None, k_tumor_allee)
 
-# TODO: Add PTH (i.e., PTHrP) synthesis rate as a function of the number of tumor cells
-
+# Make PTH (i.e., PTHrP) synthesis rate a function of the number of tumor cells
 Parameter('k_tumor_PTHrP', 5e2)  # 1/day
 alias_model_components()
 IP.expr = sympify(k_tumor_PTHrP * Tumor_tot)
@@ -85,17 +120,17 @@ IP.expr = sympify(k_tumor_PTHrP * Tumor_tot)
 
 # ...
 
-# TODO: Hypothesize that tumor cells secrete a factor that promotes OB death
+# Hypothesize that tumor cells secrete a factor that promotes OB death
 Parameter('k_tumor_OB', 0.002)  # 1/fM-day  # 0.01
 alias_model_components()
 Rule('AOB_death_tumor', Tumor() + B() >> Tumor(), k_tumor_OB)
 
-# TODO: Hypothesize that tumor cells secrete a factor that promotes OC birth
+# Hypothesize that tumor cells secrete a factor that promotes OC birth
 Parameter('k_tumor_OC', 0.004)  # 1/fM-day  # 0.01
 alias_model_components()
 Rule('AOC_creation_tumor', Tumor() >> Tumor() + C(), k_tumor_OC)
 
-# #########################
+# run simulation(s)
 
 # start with zero cells and run to equilibrium
 R_0.value = 0.0
@@ -106,19 +141,7 @@ C_0.value = 0.0
 DA.value = 2  # /day
 kB.value = 0.022  # /day
 
-# estimate rate constant for OB -> OB + Bone to get bone density ~ 100
-mean_OB = 1.06E-02 * 1000  # from data
-mean_OC = 9.16E-04 * 1000  # from data
-# k_B_builds_bone.value = (k_C_consumes_bone.value * mean_OC * Bone_0.value / mean_OB) * 2
-# k_B_builds_bone.value = (k1_C_consumes_bone.value * mean_OC * mean_OC * Bone_0.value /
-#                          (k2_C_consumes_bone.value + mean_OC)) / mean_OB * 2 * 1.65
-k1_B_builds_bone.value = k1_C_consumes_bone.value * mean_OC ** (nC.value + 1) * Bone_0.value / \
-                         (k2_C_consumes_bone.value + mean_OC ** nC.value) * \
-                         mean_OB ** (nB.value -1) / \
-                         (k2_B_builds_bone.value + mean_OB ** nB.value) * \
-                         2 * 1.2
-
-# run simulation(s)
+# create simulator object
 tspans = []
 outputs = []
 sim = ScipyOdeSimulator(model, verbose=True)
@@ -127,10 +150,10 @@ sim = ScipyOdeSimulator(model, verbose=True)
 tspans.append(np.linspace(-500, 0, 5001))  # 100, 1001)
 outputs.append(sim.run(tspan=tspans[-1]))
 
-# inject PTH (or PTHrP from tumor cells)
+# Add a small amount of tumor cells
 tspans.append(np.linspace(0, 30, 31))
 initials = outputs[-1].species[-1]
-idx = [str(sp) for sp in model.species].index('Tumor()')
+idx = [str(sp) for sp in model.species].index('Tumor()')  # get index of Tumor species
 initials[idx] = 1  # fM
 outputs.append(sim.run(tspan=tspans[-1], initials=initials))
 
@@ -162,5 +185,20 @@ for obs, ref, leg_label, color, ylabel, scale in zip(obs_to_plot, refs, leg_labe
     plt.xlim(left=0, right=4)
     plt.legend(loc=0)
     plt.tight_layout()
+
+# plot OB bone production and OC bone consumption rates over time
+plt.figure()
+plt.plot(tspans[-1]/7, outputs[-1].expressions['k_B_builds_bone'], lw=2, color='b', label=kB_label)
+plt.xlabel('time (week)')
+plt.ylabel('value (/day)')
+plt.legend(loc=0, fontsize=14)
+plt.tight_layout()
+
+plt.figure()
+plt.plot(tspans[-1]/7, outputs[-1].expressions['k_C_consumes_bone'], lw=2, color='r', label=kC_label)
+plt.xlabel('time (week)')
+plt.ylabel('value (/fM-day)')
+plt.legend(loc=0, fontsize=14)
+plt.tight_layout()
 
 plt.show()
