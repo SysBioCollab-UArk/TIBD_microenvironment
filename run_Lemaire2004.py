cac3f5b5b624a41c42b850956939245db67d1e12
AnishkaN7 <anishka768@gmail.com>
2024-05-15 18:14:18 -0500

    added if__name__=="__main__" to run_Lemaire2004.py

--- run_Lemaire2004.py
+++ run_Lemaire2004.py
@@ -138,88 +138,90 @@ alias_model_components()
 Initial(Bisphos(), Bisphos_0)
 Rule('AOC_death_bisphos', Bisphos() + C() >> Bisphos(), k_bisphos_AOC)
 
-# run simulation(s)
+if __name__ == "__main__":
 
-# start with zero cells and run to equilibrium
-R_0.value = 0.0
-B_0.value = 0.0
-C_0.value = 0.0
+    # run simulation(s)
 
-# modify some constants
-DA.value = 2  # /day
-kB.value = 0.022  # /day
+    # start with zero cells and run to equilibrium
+    R_0.value = 0.0
+    B_0.value = 0.0
+    C_0.value = 0.0
 
-# create simulator object
-tspans = []
-outputs = []
-linestyles = []
-sim = ScipyOdeSimulator(model, verbose=True)
+    # modify some constants
+    DA.value = 2  # /day
+    kB.value = 0.022  # /day
 
-# equilibration
-tspans.append(np.linspace(-500, 0, 5001))  # 100, 1001)
-outputs.append(sim.run(tspan=tspans[-1]))
-linestyles.append('-')
+    # create simulator object
+    tspans = []
+    outputs = []
+    linestyles = []
+    sim = ScipyOdeSimulator(model, verbose=True)
 
-# Add a small amount of tumor cells
-tspans.append(np.linspace(0, 30, 31))  # days
-initials = outputs[-1].species[-1]
-idx_tumor = [str(sp) for sp in model.species].index('Tumor()')  # get index of Tumor species
-initials[idx_tumor] = 1  # fM
-outputs.append(sim.run(tspan=tspans[-1], initials=initials))
-linestyles.append('-')
+    # equilibration
+    tspans.append(np.linspace(-500, 0, 5001))  # 100, 1001)
+    outputs.append(sim.run(tspan=tspans[-1]))
+    linestyles.append('-')
 
-# Add bisphosphonate (Zoledronic acid) at day 6
-tspans.append(np.linspace(6, 30, 241))  # days
-initials = outputs[-1].species[6]
-idx_bisphos = [str(sp) for sp in model.species].index('Bisphos()')  # get index of Bisphos species
-initials[idx_bisphos] = 1  # fM
-outputs.append(sim.run(tspan=tspans[-1], initials=initials))
-linestyles.append('--')
+    # Add a small amount of tumor cells
+    tspans.append(np.linspace(0, 30, 31))  # days
+    initials = outputs[-1].species[-1]
+    idx_tumor = [str(sp) for sp in model.species].index('Tumor()')  # get index of Tumor species
+    initials[idx_tumor] = 1  # fM
+    outputs.append(sim.run(tspan=tspans[-1], initials=initials))
+    linestyles.append('-')
 
-# plot time courses
-obs_to_plot = [['OB_tot'], ['C_obs'], ['Bone_tot'], ['Tumor_tot']]
-refs = [[exp_data[('Tumor', 'OB')]], [exp_data[('Tumor', 'OC')]], [exp_data[('Tumor', 'Bone')]],
-        [exp_data[('Tumor', 'Tumor')]]]
-leg_labels = [['OB'], ['OC'], ['Bone'], ['Tumor']]
-colors = [['b'], ['r'], ['g'], ['orange']]
-ylabels = ['concentration (pM)', 'concentration (pM)', 'relative BV/TV', 'concentration (pM)']
-scales = [1/1000, 1/1000, 1/Bone_0.value, 1/1000]
-for obs, ref, leg_label, color, ylabel, scale in zip(obs_to_plot, refs, leg_labels, colors, ylabels, scales):
-    print(obs)
+    # Add bisphosphonate (Zoledronic acid) at day 6
+    tspans.append(np.linspace(6, 30, 241))  # days
+    initials = outputs[-1].species[6]
+    idx_bisphos = [str(sp) for sp in model.species].index('Bisphos()')  # get index of Bisphos species
+    initials[idx_bisphos] = 1  # fM
+    outputs.append(sim.run(tspan=tspans[-1], initials=initials))
+    linestyles.append('--')
+
+    # plot time courses
+    obs_to_plot = [['OB_tot'], ['C_obs'], ['Bone_tot'], ['Tumor_tot']]
+    refs = [[exp_data[('Tumor', 'OB')]], [exp_data[('Tumor', 'OC')]], [exp_data[('Tumor', 'Bone')]],
+            [exp_data[('Tumor', 'Tumor')]]]
+    leg_labels = [['OB'], ['OC'], ['Bone'], ['Tumor']]
+    colors = [['b'], ['r'], ['g'], ['orange']]
+    ylabels = ['concentration (pM)', 'concentration (pM)', 'relative BV/TV', 'concentration (pM)']
+    scales = [1/1000, 1/1000, 1/Bone_0.value, 1/1000]
+    for obs, ref, leg_label, color, ylabel, scale in zip(obs_to_plot, refs, leg_labels, colors, ylabels, scales):
+        print(obs)
+        plt.figure()
+        for i in range(len(obs)):
+            add_label = True
+            for tspan, output, ls in zip(tspans, outputs, linestyles):
+                label = leg_label[i] if add_label else None
+                plt.plot(tspan/7, output.observables[obs[i]] * scale, lw=2, color=color[i], ls=ls, label=label)
+                add_label = False
+            if obs[i] == 'Bone_tot':
+                plt.ylim(top=1.65, bottom=-0.05)  # top=1.2
+            # plot reference (experimental) data
+            if ref[i] is not None:
+                plt.errorbar(*ref[i], ecolor='k', ls='None', marker='o', ms=8,  capsize=10, color=color[i],
+                             label='experiment')
+        plt.xlabel('time (week)')
+        plt.ylabel(ylabel)
+        plt.xlim(left=0, right=4)
+        plt.legend(loc=0)
+        plt.tight_layout()
+
+    # plot OB bone production and OC bone consumption rates over time
     plt.figure()
-    for i in range(len(obs)):
-        add_label = True
-        for tspan, output, ls in zip(tspans, outputs, linestyles):
-            label = leg_label[i] if add_label else None
-            plt.plot(tspan/7, output.observables[obs[i]] * scale, lw=2, color=color[i], ls=ls, label=label)
-            add_label = False
-        if obs[i] == 'Bone_tot':
-            plt.ylim(top=1.65, bottom=-0.05)  # top=1.2
-        # plot reference (experimental) data
-        if ref[i] is not None:
-            plt.errorbar(*ref[i], ecolor='k', ls='None', marker='o', ms=8,  capsize=10, color=color[i],
-                         label='experiment')
+    plt.plot(tspans[-2]/7, outputs[-2].expressions['k_B_builds_bone'], lw=2, color='b', label=kB_label)
+    plt.plot(tspans[-1]/7, outputs[-1].expressions['k_B_builds_bone'], lw=2, color='b', ls='--')
     plt.xlabel('time (week)')
-    plt.ylabel(ylabel)
-    plt.xlim(left=0, right=4)
-    plt.legend(loc=0)
+    plt.ylabel('value (/day)')
+    plt.legend(loc=0, fontsize=14)
     plt.tight_layout()
 
-# plot OB bone production and OC bone consumption rates over time
-plt.figure()
-plt.plot(tspans[-2]/7, outputs[-2].expressions['k_B_builds_bone'], lw=2, color='b', label=kB_label)
-plt.plot(tspans[-1]/7, outputs[-1].expressions['k_B_builds_bone'], lw=2, color='b', ls='--')
-plt.xlabel('time (week)')
-plt.ylabel('value (/day)')
-plt.legend(loc=0, fontsize=14)
-plt.tight_layout()
+    plt.figure()
+    plt.plot(tspans[-2]/7, outputs[-2].expressions['k_C_consumes_bone'], lw=2, color='r', label=kC_label)
+    plt.plot(tspans[-1]/7, outputs[-1].expressions['k_C_consumes_bone'], lw=2, color='r', ls='--')
+    plt.xlabel('time (week)')
+    plt.ylabel('value (/fM-day)')
+    plt.legend(loc=0, fontsize=14)
+    plt.tight_layout()
 
-plt.figure()
-plt.plot(tspans[-2]/7, outputs[-2].expressions['k_C_consumes_bone'], lw=2, color='r', label=kC_label)
-plt.plot(tspans[-1]/7, outputs[-1].expressions['k_C_consumes_bone'], lw=2, color='r', ls='--')
-plt.xlabel('time (week)')
-plt.ylabel('value (/fM-day)')
-plt.legend(loc=0, fontsize=14)
-plt.tight_layout()
-
-plt.show()
+    plt.show()
