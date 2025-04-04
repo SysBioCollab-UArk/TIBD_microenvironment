c3811876b3d2b2dfd8c713dca6c3053c7303fccd
lh64 <lh64@cornell.edu>
2025-04-04 05:50:03 -0500

    Made some small changes to make_tc_plots.py and removed the zero time point from TIBD_Johnson2011.csv and TIBD_PopD_Data.csv, since the tumor cell concentration at that point is 1 fM, not 0

--- make_tc_plots.py
+++ make_tc_plots.py
@@ -3,97 +3,97 @@ import matplotlib.pyplot as plt
 import os
 from matplotlib.ticker import ScalarFormatter
 
-path = os.path.join('MODELS', 'SAVE', 'RUN_4')
 
-# Experimental data
-expt_data_file = os.path.join(path, 'TIBD_PopD_Data.csv')
-expt_data_raw = np.genfromtxt(expt_data_file, dtype=None, delimiter=',', names=True, encoding="utf_8_sig")
-print(expt_data_raw.dtype.names)
-expt_ids = list(dict.fromkeys(expt_data_raw['expt_id']))
-print(expt_ids)
-# gather experimental data for all alt_expt_ids
-expt_data = {}
-n_alt_expt_ids = {}
-for expt_id in expt_ids:
-    alt_expt_ids = list(dict.fromkeys([d['alt_expt_id'] for d in expt_data_raw if d['expt_id'] == expt_id]))
-    n_alt_expt_ids[expt_id] = len(alt_expt_ids)
-    for alt_expt_id in alt_expt_ids:
-        expt_data[alt_expt_id] = \
-            np.array([d for d in expt_data_raw if d['expt_id'] == expt_id and d['alt_expt_id'] == alt_expt_id])
+def make_fig(path, sim_expt_ids, fig_name, expt_data_only=True):
 
-print('n_alt_expt_ids:', n_alt_expt_ids)
-for key in expt_data.keys():
-    print(key)
-    # print('    ', expt_data[key].dtype.names)
-    # for d in expt_data[key]:
-    #     print('    ', d)
-# quit()
+    # Experimental data
+    expt_data_file = os.path.join(path, 'TIBD_PopD_Data.csv')
+    expt_data_raw = np.genfromtxt(expt_data_file, dtype=None, delimiter=',', names=True, encoding="utf_8_sig")
+    print(expt_data_raw.dtype.names)
+    expt_ids = list(dict.fromkeys(expt_data_raw['expt_id']))
+    print(expt_ids)
+    # gather experimental data for all alt_expt_ids
+    expt_data = {}
+    n_alt_expt_ids = {}
+    for expt_id in expt_ids:
+        alt_expt_ids = list(dict.fromkeys([d['alt_expt_id'] for d in expt_data_raw if d['expt_id'] == expt_id]))
+        n_alt_expt_ids[expt_id] = len(alt_expt_ids)
+        for alt_expt_id in alt_expt_ids:
+            expt_data[alt_expt_id] = \
+                np.array([d for d in expt_data_raw if d['expt_id'] == expt_id and d['alt_expt_id'] == alt_expt_id])
 
-print('----------')
+    print('n_alt_expt_ids:', n_alt_expt_ids)
+    for key in expt_data.keys():
+        print(key)
+        # print('    ', expt_data[key].dtype.names)
+        # for d in expt_data[key]:
+        #     print('    ', d)
+    # quit()
 
-# Simulation data
-sim_data_file = os.path.join(path, 'SIM_DATA.csv')
-sim_data_raw = np.genfromtxt(sim_data_file, dtype=None, delimiter=',', names=True, encoding="utf_8_sig")
-print(sim_data_raw.dtype.names)
-sim_ids = list(dict.fromkeys(sim_data_raw['sim_id']))
-print(sim_ids)
-# gather simulation data for all alt_expt_ids
-sim_data = {}
-for sim_id in sim_ids:
-    if sim_id in expt_ids:
-        # get all data points
-        data = np.array([d for d in sim_data_raw if d['sim_id'] == sim_id])
-        # number of experiments and observables
-        n_expts = n_alt_expt_ids[sim_id]
-        n_obs = len(np.unique(data['observable']))
-        # calculate the number of time points
-        n_timepoints = len(data) // (n_expts * n_obs)
-        # extract indices for each experiment
-        for exp in range(n_expts):
-            indices = np.hstack([
-                np.arange(start_idx, start_idx + n_timepoints)  # Select contiguous blocks
-                for start_idx in range(exp * n_timepoints, len(data), n_expts * n_timepoints)
-            ])
-            alt_expt_id = list(expt_data.keys())[len(sim_data)]
-            sim_data[alt_expt_id] = data[indices]
-    else:
-        sim_data[sim_id] = np.array([d for d in sim_data_raw if d['sim_id'] == sim_id])
+    print('----------')
 
-for key in sim_data.keys():
-    print(key)
-    # print('    ', sim_data[key].dtype.names)
-    # for d in sim_data[key][key2]:
-    #     print('    ', d)
-# quit()
+    # Simulation data
+    sim_data_file = os.path.join(path, 'SIM_DATA.csv')
+    sim_data_raw = np.genfromtxt(sim_data_file, dtype=None, delimiter=',', names=True, encoding="utf_8_sig")
+    print(sim_data_raw.dtype.names)
+    sim_ids = list(dict.fromkeys(sim_data_raw['sim_id']))
+    print(sim_ids)
+    # gather simulation data for all alt_expt_ids
+    sim_data = {}
+    for sim_id in sim_ids:
+        if sim_id in expt_ids:
+            # get all data points
+            data = np.array([d for d in sim_data_raw if d['sim_id'] == sim_id])
+            # number of experiments and observables
+            n_expts = n_alt_expt_ids[sim_id]
+            n_obs = len(np.unique(data['observable']))
+            # calculate the number of time points
+            n_timepoints = len(data) // (n_expts * n_obs)
+            # extract indices for each experiment
+            for exp in range(n_expts):
+                indices = np.hstack([
+                    np.arange(start_idx, start_idx + n_timepoints)  # Select contiguous blocks
+                    for start_idx in range(exp * n_timepoints, len(data), n_expts * n_timepoints)
+                ])
+                alt_expt_id = list(expt_data.keys())[len(sim_data)]
+                sim_data[alt_expt_id] = data[indices]
+        else:
+            sim_data[sim_id] = np.array([d for d in sim_data_raw if d['sim_id'] == sim_id])
 
-print('----------')
+    for key in sim_data.keys():
+        print(key)
+        # print('    ', sim_data[key].dtype.names)
+        # for d in sim_data[key][key2]:
+        #     print('    ', d)
+    # quit()
 
-obs_names = np.unique(sim_data_raw['observable'])
-print(obs_names)
+    print('----------')
 
-print('----------')
+    obs_names = np.unique(sim_data_raw['observable'])
+    print(obs_names)
 
-obs_labels = \
-    {'Bone_tot': 'Bone Density',
-     'C_obs': 'Osteoclasts',
-     'OB_tot': 'Osteoblasts',
-     'Tumor_tot': 'Tumor Cells'}
+    print('----------')
 
-legend_labels = \
-    {'Bennett2024': 'This work: No Drug-Tumor',
-     'Johnson2011 (PBS-Tumor)': 'Johnson (2011): No Drug-Tumor',
-     'Johnson2011 (ZA-Tumor)': 'Johnson (2011): ZA-Tumor',
-     'Johnson2011 (ZA-NoTumor)': 'Johnson (2011): ZA-No Tumor',
-     '3': 'This work: ZA-Tumor'}
+    obs_labels = \
+        {'Bone_tot': 'Bone Density',
+         'C_obs': 'Osteoclasts',
+         'OB_tot': 'Osteoblasts',
+         'Tumor_tot': 'Tumor Cells'}
 
-cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']  # standard colors
-colors = {'Bennett2024': cycle[0],
-          'Johnson2011 (PBS-Tumor)': cycle[1],
-          'Johnson2011 (ZA-Tumor)': cycle[2],
-          'Johnson2011 (ZA-NoTumor)': cycle[3],
-          '3': cycle[4]}
+    legend_labels = \
+        {'Bennett2024': 'This work: No Drug-Tumor',
+         'Johnson2011 (PBS-Tumor)': 'Johnson (2011): No Drug-Tumor',
+         'Johnson2011 (ZA-Tumor)': 'Johnson (2011): ZA-Tumor',
+         'Johnson2011 (ZA-NoTumor)': 'Johnson (2011): ZA-No Tumor',
+         '3': 'This work: ZA-Tumor'}
 
-def make_fig(sim_expt_ids, fig_name, expt_data_only=True):
+    cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']  # standard colors
+    colors = {'Bennett2024': cycle[0],
+              'Johnson2011 (PBS-Tumor)': cycle[1],
+              'Johnson2011 (ZA-Tumor)': cycle[2],
+              'Johnson2011 (ZA-NoTumor)': cycle[3],
+              '3': cycle[4]}
+
     nrows = 2
     ncols = 2
     fig, axs = plt.subplots(nrows, ncols, sharex='all', figsize=[1*6.4, 1.2*4.8], constrained_layout=True)
@@ -110,9 +110,8 @@ def make_fig(sim_expt_ids, fig_name, expt_data_only=True):
         axs[row, col].set_title(obs_labels[obs_name], fontweight='bold', fontsize=14)
         for sim_expt_id in sim_expt_ids:
             # plot Johnson et al. (2011) tumor data on 2nd y-axis
-            ax = axs_tumor2 \
-                if obs_name == 'Tumor_tot' and \
-                   (sim_expt_id == 'Johnson2011 (PBS-Tumor)' or sim_expt_id == 'Johnson2011 (ZA-Tumor)') \
+            ax = axs_tumor2 if obs_name == 'Tumor_tot' and \
+                               (sim_expt_id == 'Johnson2011 (PBS-Tumor)' or sim_expt_id == 'Johnson2011 (ZA-Tumor)') \
                 else axs[row, col]
             # check if experimental data exists
             expt_time = [] if sim_expt_id not in expt_data.keys() \
@@ -124,6 +123,15 @@ def make_fig(sim_expt_ids, fig_name, expt_data_only=True):
                 yval_max = [d['yval_max'] for d in sim_data[sim_expt_id] if d['observable'] == obs_name]
                 ax.fill_between(sim_time, yval_min, yval_max, alpha=0.25, label=legend_labels[sim_expt_id],
                                            color=colors[sim_expt_id])
+                # add an arrow pointing to the right for the Johnson et al. (PBS-Tumor) Day 14 data point
+                if obs_name == 'Tumor_tot' and sim_expt_id == 'Johnson2011 (PBS-Tumor)':
+                    from matplotlib.patches import FancyArrowPatch
+                    # Shaft (dotted line)
+                    ax.plot([14, ax.get_xlim()[-1]], [1, 1], linestyle='--', color=colors[sim_expt_id], lw=1.5)
+                    # Head (small arrow overlay)
+                    arrow = FancyArrowPatch(posA=(ax.get_xlim()[-1] - 0.5, 1), posB=(ax.get_xlim()[-1], 1),
+                                            arrowstyle='->', color=colors[sim_expt_id], lw=1.5, mutation_scale=10)
+                    ax.add_patch(arrow)
             # plot experimental data
             if len(expt_time) > 0:
                 average = [d['average'] for d in expt_data[sim_expt_id] if d['observable'] == obs_name]
@@ -154,17 +162,32 @@ def make_fig(sim_expt_ids, fig_name, expt_data_only=True):
     left = 0
     bottom = 0.12
     fig.get_layout_engine().set(rect=(left, bottom, 1 - left, 1 - bottom))  # (left, bottom, width, height)
-    plt.savefig(fig_name, format='pdf')
+    # save the figure
+    fig.savefig(fig_name)
 
 
 if __name__ == '__main__':
 
-    make_fig(['Bennett2024', 'Johnson2011 (PBS-Tumor)', 'Johnson2011 (ZA-Tumor)', 'Johnson2011 (ZA-NoTumor)'],
-             'Figure_3.pdf', expt_data_only=True)
-    make_fig(['Bennett2024', 'Johnson2011 (PBS-Tumor)'], 'Figure_4.pdf', expt_data_only=False)
-    # make_fig(['Bennett2024', '3'], 'Figure_5.pdf', expt_data_only=False)
-    # make_fig(['Johnson2011 (ZA-Tumor)', '3'], 'Figure_ZA.pdf', expt_data_only=False)
-    make_fig(['Johnson2011 (PBS-Tumor)', 'Johnson2011 (ZA-Tumor)'],
-             'Figure_B_C.pdf', expt_data_only=False)
+    base_path = os.path.join('MODELS', 'SAVE', 'Leonard')
+
+    directories = [
+        'A_Errors_Orig_ALL_NoZeroPt',
+        'B_Errors_5pct_ALL_NoZeroPt',
+        'C_Errors_Orig_TUMOR_5pct_OTHERS_WithZeroPt'
+    ]
+
+    A = 'Bennett2024'
+    B = 'Johnson2011 (PBS-Tumor)'
+    C = 'Johnson2011 (ZA-Tumor)'
+    D = 'Johnson2011 (ZA-NoTumor)'
+
+    for d in directories:
+        path = os.path.join(base_path, d)
+
+        make_fig(path, [A, B, C, D], 'Figure_3', expt_data_only=True)
+        make_fig(path, [A, B], 'Figure_4', expt_data_only=False)
+        # make_fig(path, [A, '3'], 'Figure_5', expt_data_only=False)
+        # make_fig(path, [C, '3'], 'Figure_ZA', expt_data_only=False)
+        make_fig(path, [B, C], 'Figure_B_C', expt_data_only=False)
 
     plt.show()
