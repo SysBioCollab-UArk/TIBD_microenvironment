552916e8fd136a9b5e516521a19b57e6c65eedf9
lh64 <lh64@cornell.edu>
2025-06-12 07:07:04 -0500

    Changed the Tumor_tot data for the Johnson et al. (2011) data set and updated run_tibd_pydream.py to work with the new data. Specifically, the tumor data is now given in fM units based on estimated exponential curve fits, assuming the concentration at day 0 is 1 fM (the injected amount). This means that PBS-Tumor, ZA-Tumor, and ZA-NoTumor are again being treated as separate experiments using the `SequentialInjections` simulation protocol class.
    Also, changed the concentrations at Days 0 and 7 for the Bennett2024 tumor data from 0 to 1. This shouldn't have any appreciable effect but am doing it for completeness.

--- run_tibd_pydream.py
+++ run_tibd_pydream.py
@@ -10,13 +10,14 @@ add_bisphosphonate_components()
 
 solver = ScipyOdeSimulator(model)
 
-# Experiment A
+# Experiments A & B
 tumor_injection = SequentialInjections(solver, t_equil=500, time_perturb_value={0: ('Tumor()', 1)})
 
-# Experiment B
-# tumor_bisphos_injection = SequentialInjections(solver, t_equil=500,
-#                                                perturb_time_amount={'Tumor()': (0, 1), 'Bisphos()': (6, 1)})
-time_perturb_value = [{0: ('Tumor()', 1)},
+# Experiment C
+tumor_bisphos_injection = SequentialInjections(solver, t_equil=500,
+                                               time_perturb_value={0: ('Tumor()', 1), 6: ('Bisphos()', 1)})
+
+'''time_perturb_value = [{0: ('Tumor()', 1)},
                       {0: ('Tumor()', 1), 6: ('Bisphos()', 1)}]
 # scale 'Tumor_tot' by value at t=14 in expt 0 (set a detection threshold)
 def divmax(value, scale):
@@ -26,29 +27,33 @@ def divmax(value, scale):
 scale_by_eidx_time = \
         {'Tumor_tot': {'eidx': 0, 'time': 14, 'scale_func': divmax}}
 multi_exp_injection = ParallelExperiments(solver, t_equil=500, time_perturb_value=time_perturb_value,
-                                          scale_by_eidx_time=scale_by_eidx_time)
+                                          scale_by_eidx_time=scale_by_eidx_time)'''
 
-# Experiment C
+# Experiment D
 bisphos_injection = SequentialInjections(solver, t_equil=500, time_perturb_value={6: ('Bisphos()', 1)})
 
+# Additional parameters
 custom_priors = {'N': ('uniform', 0.3)}  # , 'nB': ('norm', 1), 'nC': ('norm', 1)}
-no_sample = ['R_0', 'B_0', 'C_0', 'f0', 'IL', 'IO', 'IP_const', 'Bone_0', 'Tumor_0', 'CC_ON', 'nB', 'nC',
-             'ALLEE_ON', 'A', 'Bisphos_0']
-obs_labels = {'Bone_tot': 'bone density', 'C_obs': 'osteoclasts', 'OB_tot': 'osteoblasts',
-              'Tumor_tot': 'tumor cells'}
 
-param_expts_map = {'k_tumor_div_basal': [['A'], ['B', 'C']],  #, 'D']],
-                   'k_tumor_dth': [['A'], ['B', 'C']],  #, 'D']],
-                   'k_tumor_div_TGFb': [['A'], ['B', 'C']],  #, 'D']],
-                   'k_tumor_PTHrP': [['A'], ['B', 'C']],  #, 'D']],
-                   'k_tumor_OB': [['A'], ['B', 'C']],  #, 'D']],
-                   'k_tumor_OC': [['A'], ['B', 'C']]}  #, 'D']]}
+no_sample = ['R_0', 'B_0', 'C_0', 'f0', 'IL', 'IO', 'IP_const', 'Bone_0', 'Tumor_0', 'CC_ON', 'nB', 'nC', 'ALLEE_ON',
+             'A', 'Bisphos_0']
+
+obs_labels = {'Bone_tot': 'bone density', 'C_obs': 'osteoclasts', 'OB_tot': 'osteoblasts', 'Tumor_tot': 'tumor cells'}
+
+param_expts_map = {
+    'k_tumor_div_basal': [['A'], ['B', 'C', 'D']],
+    'k_tumor_dth': [['A'], ['B', 'C', 'D']],
+    'k_tumor_div_TGFb': [['A'], ['B', 'C', 'D']],
+    'k_tumor_PTHrP': [['A'], ['B', 'C', 'D']],
+    'k_tumor_OB': [['A'], ['B', 'C', 'D']],
+    'k_tumor_OC': [['A'], ['B', 'C', 'D']]
+}
 
 if __name__ == '__main__':
 
     calibrator = ParameterCalibration(model,
                                       exp_data_file,
-                                      [tumor_injection, multi_exp_injection, bisphos_injection],
+                                      [tumor_injection] * 2 + [tumor_bisphos_injection, bisphos_injection],
                                       priors=custom_priors,
                                       no_sample=no_sample,
                                       param_expts_map=param_expts_map)
