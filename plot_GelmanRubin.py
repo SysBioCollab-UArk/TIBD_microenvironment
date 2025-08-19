2715dd39eee0a57b972e8f2d9a7733161fb60ec9
lh64 <lh64@cornell.edu>
2025-08-19 04:05:11 -0500

    A couple of small changes to plot_GelmanRubin.py

--- plot_GelmanRubin.py
+++ plot_GelmanRubin.py
@@ -4,15 +4,15 @@ import glob
 import os
 import re
 
-# dir = 'Bennett2024_Johnson2011'
-dir = 'Bennett2024_Johnson2011_10pct_15pct'
+dir = 'Bennett2024_Johnson2011_stderr_2abs'
 path = os.path.join('MODELS', 'SAVE', 'Leonard', dir)
 
-files = glob.glob(os.path.join(path, '*Gelman*'))
-print(files)
+# dir = 'Messika'
+# path = os.path.join('..', 'AorticCalcification', 'SAVE', dir)
 
+files = glob.glob(os.path.join(path, '*Gelman*'))
 iterations = sorted([int(re.search("\d\d+", os.path.split(file)[1]).group()) for file in files])
-print(iterations)
+print('iterations:', iterations)
 
 GR = []
 for i in iterations:
@@ -25,13 +25,13 @@ colors = [cmap(i / (GR.shape[0] - 1)) for i in range(GR.shape[0])]
 
 labels = ['p%d' % i for i in range(GR.shape[0])]
 
-plt.figure(constrained_layout=True)
+plt.figure(constrained_layout=True, figsize=(6.4 * 2, 4.8))
 for GR_array, color, label in zip(GR, colors, labels):
     plt.plot(iterations, GR_array, '-', color=color, label=label)
 plt.ticklabel_format(axis='x', style='sci', scilimits=(0,0))
 plt.axhline(1.2, ls='--', color='black', lw=2)
 plt.xlabel('Iterations')
 plt.ylabel('Gelman-Rubin metric')
-plt.legend(loc='best', ncol=4)
+plt.legend(loc='best', ncol=4, bbox_to_anchor=(1.02, 1))
 
 plt.show()
