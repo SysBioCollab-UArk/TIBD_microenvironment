7a7d6a2b4c3bebf5a664cd62c94280ed275d04c2
lh64 <lh64@cornell.edu>
2025-12-31 20:12:02 -0600

    Modified comments in perturbations.py to clarify the units being used for the bisphosphonate species

--- MODULES/perturbations.py
+++ MODULES/perturbations.py
@@ -4,8 +4,8 @@ from pysb.util import alias_model_components
 
 def add_bisphosphonate_components():
     Monomer('Bisphos')
-    Parameter('Bisphos_0', 0)  # fM
-    Parameter('k_bisphos_AOC', 1)  # 1/fM-day
+    Parameter('Bisphos_0', 0)  # unitless (ratio of dose to baseline dose = 0.1 mg/mouse)
+    Parameter('k_bisphos_AOC', 1)  # 1/day (ZA is unitless, not in fM)
     alias_model_components()
     Initial(Bisphos(), Bisphos_0)
     Rule('AOC_death_bisphos', Bisphos() + C() >> Bisphos(), k_bisphos_AOC)
